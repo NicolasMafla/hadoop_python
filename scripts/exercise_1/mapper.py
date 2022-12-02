115d932229eed60df2313cda17adf0feba1a49e1
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-02 18:41:22 -0500

    mapper updated and reducer added

--- scripts/exercise_1/mapper.py
+++ scripts/exercise_1/mapper.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
-
 import sys
+import re
 
+# Read each line from stdin
 for line in sys.stdin:
 
+    # Get the words in each line
     words = line.split()
+
+    # Generate the count for each word
     for word in words:
-        print('{0}\t{1}'.format(word, 1))
+        # Write the key-value pair to stdout to be processed by
+        # the reducer.
+        # The key is anything before the first tab character and the
+        # value is anything after the first tab character.
+        word = word.strip()
+        word = re.sub(r"[^\w\s]", "", word)
+        word = word.lower()
+        print("{0}\t{1}".format(word, 1))
