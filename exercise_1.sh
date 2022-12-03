f78512d4a81155d8f68a4c75241f63249798ecc0
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-02 20:21:23 -0500

    exercise 1 sh updated

--- exercise_1.sh
+++ exercise_1.sh
@@ -1,1 +1,1 @@
-cat ./data/casoDePruebaEJ1.txt | ./scripts/exercise_1/mapper.py | sort -k 1
+cat ./data/casoDePruebaEJ1.txt | ./scripts/exercise_1/mapper.py | sort -k 1 | ./scripts/exercise_1/reducer.py
