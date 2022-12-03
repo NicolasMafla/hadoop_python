c7bc99461b3908eaa21cba6bdcb0485438443c09
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-02 19:05:57 -0500

    exercise 1 mapper updated

--- scripts/exercise_1/mapper.py
+++ scripts/exercise_1/mapper.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python
 import sys
-import re
 
-# Read each line from stdin
 for line in sys.stdin:
-
-    # Get the words in each line
-    words = line.split()
-
-    # Generate the count for each word
-    for word in words:
-        # Write the key-value pair to stdout to be processed by
-        # the reducer.
-        # The key is anything before the first tab character and the
-        # value is anything after the first tab character.
-        word = word.strip()
-        word = re.sub(r"[^\w\s]", "", word)
-        word = word.lower()
-        print("{0}\t{1}".format(word, 1))
+    person, spent = line.split()
+    info = dict()
+    print(f"{person}:{spent}")
