287e211f8806c053fcc0400d25495d3c5b78641e
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-03 20:26:06 -0500

    exercise 1 sh updated

--- exercise_1.sh
+++ exercise_1.sh
@@ -1,1 +1,1 @@
-cat ./data/casoDePruebaEJ1.txt | ./scripts/exercise_1/mapper.py | sort -k 1 | ./scripts/exercise_1/reducer.py
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming3.3.4.jar -files ./hadoop_python/scripts/exercise_1/mapper.py,./hadoop_python/scripts/exercise_1/reducer.py -mapper ./hadoop_python/scripts/exercise_1/mapper.py -reducer ./hadoop_python/scripts/exercise_1/reducer.py -input ./hadoop_python/data/casoDePruebaEJ1.txt -output ./hadoop_python/outputs/exercise_1
