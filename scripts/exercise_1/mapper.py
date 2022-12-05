c55bf0500b90c50d693f33a8d21ea9e3f5a263ec
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-04 21:18:15 -0500

    exercise 1mapper and reducer updated

--- scripts/exercise_1/mapper.py
+++ scripts/exercise_1/mapper.py
@@ -3,5 +3,4 @@ import sys
 
 for line in sys.stdin:
     person, spent = line.split()
-    info = dict()
     print(f"{person}\t{spent}")
--- scripts/exercise_1/reducer.py
+++ scripts/exercise_1/reducer.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3.8
 import sys
 
+
+def most_frequent(lst: list) -> int:
+    return max(set(lst), key=lst.count)
+
+
 current_person = None
 spends = []
 
 for index, line in enumerate(sys.stdin):
     person, spent = line.split("\t")
     if index == 0:
-      current_person = person
+        current_person = person
     if current_person == person:
-      spends.append(int(spent.strip()))
+        spends.append(int(spent.strip()))
     else:
-      print(f"{current_person}\t{spends}")
-      current_person = person
-      spends = [int(spent.strip())]
-print(f"{current_person}\t{spends}")
+        print(f"{current_person}\t{most_frequent(spends)}")
+        current_person = person
+        spends = [int(spent.strip())]
+print(f"{current_person}\t{most_frequent(spends)}")
