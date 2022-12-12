4629f3d7ca8640f756e3250dfd3d18b67c7c9dbf
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-11 19:38:24 -0500

    refactoring bash script exercise 1

--- exercise_1.sh
+++ exercise_1.sh
@@ -1,1 +1,6 @@
-hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.4.jar -files ./hadoop_python/scripts/exercise_1/mapper.py,./hadoop_python/scripts/exercise_1/reducer.py -mapper ./hadoop_python/scripts/exercise_1/mapper.py -reducer ./hadoop_python/scripts/exercise_1/reducer.py -input ./hadoop_python/data/casoDePruebaEJ1.txt -output ./hadoop_python/outputs/exercise_1
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.4.jar \
+-files ./hadoop_python/scripts/exercise_1/mapper.py,./hadoop_python/scripts/exercise_1/reducer.py \
+-mapper ./hadoop_python/scripts/exercise_1/mapper.py \
+-reducer ./hadoop_python/scripts/exercise_1/reducer.py \
+-input ./hadoop_python/data/casoDePruebaEJ1.txt \
+-output ./hadoop_python/outputs/exercise_1
