0c7f001f6d9f388bf4cf13dbe77af3607e41cdb3
Nicolás Mafla <nicolas.mafla@altscore.ai>
2022-12-13 14:11:50 -0500

    change list to set

--- scripts/exercise_2/reducer.py
+++ scripts/exercise_2/reducer.py
@@ -2,14 +2,14 @@
 import sys
 
 current_citted = None
-cittings = []
+cittings = set()
 
 for index, line in enumerate(sys.stdin):
     citted, citting = line.split("\t")
     if index == 0:
         current_citted = citted
     if current_citted == citted:
-        cittings.append(int(citting.strip()))
+        cittings.add(int(citting.strip()))
     else:
         print(f"{current_citted.strip()}\t{','.join([str(s) for s in sorted(cittings)])}")
         current_citted = citted
